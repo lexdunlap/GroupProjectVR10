0308dff5489fd83bf97d1f011ba128fb67e99b15
Matt Hivner <mh2296@uncw.edu>
2017-02-26 22:28:46 -0500

    Baked ground. Character sees nearby objects

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharController : MonoBehaviour
+{
+    public List<Collider> nearMe = new List<Collider>();
+    public Text localToMe;
+
+    private StringBuilder listNearMe = new StringBuilder();
+
+    void Start()
+    {
+        updateLocals();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TurnTo(GameObject obj)
+    {
+        transform.LookAt(obj.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Boom, entered!");
+        if (!nearMe.Contains(other))
+        {
+            nearMe.Add(other);
+        }
+        updateLocals();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Boom, exited!");
+        if (nearMe.Contains(other))
+        {
+            nearMe.Remove(other);
+        }
+        updateLocals();
+    }
+
+    private void updateLocals()
+    {
+        listNearMe.Remove(0, listNearMe.Length);
+        for(int i = 0; i < nearMe.Count; i++)
+        {
+            if (nearMe[i].tag != "Untagged")
+            {
+                listNearMe.Append(nearMe[i] + "\n");
+            }
+        }
+        localToMe.text = "Objects: \n" + listNearMe;
+    }
+
+
+}
